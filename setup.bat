eeccb89d706f3c35243ce5a78fdd2643383ea721
Febrianto Bagus Setiawan <febriantobagussetiawan@gmail.com>
2022-09-19 15:26:22 +0700

    Run this first

---
+++ setup.bat
@@ -0,0 +1,10 @@
+@echo off
+color 0A
+cls
+echo Downloading Modules...
+python -m pip install colorama
+python -m pip install selenium
+python -m pip install time
+python -m pip install os
+echo Done
+pause
